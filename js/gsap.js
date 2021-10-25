ac5b105fa213ff648628b7bcdd75ccace8d942c3
BobanMadzoski <boby_madzosky@hotmail.com>
2021-10-25 14:44:58 +0200

    animations update

--- js/gsap.js
+++ js/gsap.js
@@ -3,11 +3,8 @@ window.onload = function() {
 
 
 gsap.registerPlugin(ScrollTrigger);
-// ScrollTrigger.batch(".reveal-section div", {onEnter: batch => gsap.fromTo(batch, {  duration:0.5 , opacity: 0, stagger: 0.1, ease: " power1.out" }, { duration: 0.5, opacity: 1, stagger: 0.1, ease: " power1.out"}), });
-ScrollTrigger.batch(".pic-col img", {onEnter: batch=> gsap.from(batch, {duration: 1.5,
-  x: 200,
-  autoAlpha: 0,
-  ease: Power3.out, stagger: 0.3})})
+ScrollTrigger.batch("h2, h3, h4, h5, h6", {onEnter: batch => gsap.from(batch, {  duration:1 , opacity: 0, x:100, stagger: 0.1, ease: " power4.out" })});
+
 
 const sections = gsap.utils.toArray(".panel");
 const container = document.querySelector('body');
@@ -39,6 +36,9 @@ const container = document.querySelector('body');
       stagger: 0.3
     })
 
+    
+
+
 
     gsap.fromTo(".breakline", { delay: 2,  duration: 0.7, opacity: 0, width: 0}, { delay: 2,duration: 0.7, opacity: 1, width: '100%', ease: "slow (0.3 , 0.4, false)"})
     // gsap.fromTo(".vid", {delay: 1.5, duration: 1, height: 0},{delay: 1.5, duration: 1, height: '70vh', ease: "slow (0.3, 0.4, false"})
@@ -53,33 +53,41 @@ const container = document.querySelector('body');
 
     gsap.fromTo(".header-buttons > button", { delay: 2.3,  duration: 0.3, opacity: 0 ,stagger: 0.1, ease: "slow (0.9 , 4, false)" }, { delay: 2.3,duration: 0.3, opacity: 1,stagger: 0.1,  ease: "slow (0.9 , 4, false)"})
 
+
+      // gsap.from("h2 span>span", {
+      //   duration: 1.5,
+      //   delay: 1.5,
+      //   y: 200,
+      //   autoAlpha: 0,
+      //   ease: Power3.out,
+      //   stagger: 0.3
+      // })
+
+
+
+      const childSplit = new SplitText("h2", {
+        type: "lines",
+        linesClass: "split-child"
+      });
+      const parentSplit = new SplitText("h2", {
+        // type: "lines",
+        linesClass: "split-parent"
+      });
+      
+      gsap.from(childSplit.lines, {
+        delay: 5,
+        duration: 1.5,
+        yPercent: 100,
+        ease: "power4",
+        stagger: 0.1
+      });
+      
+
+     
+
 }
 
-
-gsap.registerPlugin(ScrollTrigger);
-let revealContainers = document.querySelectorAll(".reveal");
-
-revealContainers.forEach((container) => {
-  let image = container.querySelector("img");
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: container,
-      toggleActions: "restart none none reset"
-    }
-  });
-
-  tl.set(container, { autoAlpha: 1 });
-  tl.from(container, 1.5, {
-    xPercent: -100,
-    ease: Power2.out
-  });
-  tl.from(image, 1.5, {
-    xPercent: 100,
-    scale: 1.3,
-    delay: -1.5,
-    ease: Power2.out
-  });
-});
+ScrollTrigger.batch(".reveal", {onEnter: batch => gsap.to(batch, {  duration:1 , height: 0, stagger: 0.3 })});
 
 // gsap.from(".h2-intro", {
 //   scrollTrigger:{
